684a56bf08c5d20dc5a7c290445748298fa5edce
William Paul Liggett <junktext@junktext.com>
2024-03-10 14:04:41 -0400

    [ci skip] SemVer: Improved the Bash script.

--- .version_update.sh
+++ .version_update.sh
@@ -4,6 +4,9 @@
 #
 # By: William Paul Liggett (https://junktext.com)
 
+# For script debugging:
+set -x
+
 # Prerequisite: Ensure a file called `.yarnrc` exists in the main folder (above 'src/') with at least:
 #version-git-tag false
 # This will disable Yarn v1's default behavior to update only the `package.json` file with the new version,
@@ -22,40 +25,41 @@ echo "@}-;--- Updating the app's version as shown in the web frontend at: $pub_v
 # In the React code that gets shown to the browser, there is a line that says (with an example):
 #   Modified by junktext (v1.0.21)
 # So, the following will update that portion of text with the new version number as (v1.0.22):
-sed -iE "s/(v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+)/(v$new_app_ver)/" $pub_ver_file
+sed -iE "s/(v[[:digit:]]+.[[:digit:]]+.[[:digit:]]+)/(v$new_app_ver)/" "$pub_ver_file"
 
 echo "@}-;--- Performing a Git commit and tag to save the changes."
 
 # We can then make a commit which will have the changes of the `package.json` and that `app.js` file.
-git add package.json $pub_ver_file
+git add package.json "$pub_ver_file"
 git commit -m "Version: $new_app_ver"
 
 # Next, we can then create a Git tag to reflect this as well:
-git tag -a $new_app_ver -m "Version: $new_app_ver"
+git tag -a "$new_app_ver" -m "Version: $new_app_ver"
 
 # For sanity's sake, we'll confirm we want to publicly perform the Git commit/tag.
-git show $new_app_ver
+git show "$new_app_ver"
 
-echo "@}-;--- The changes, shown above, are scheduled to be pushed to the remote Git repo..."
+echo -e "\n@}-;--- The changes, shown above, are scheduled to be pushed to the remote Git repo..."
 
 # Gets user input and stores it as $update_remote_repo
-read -p "CONFIRMATION --> Do you wish to proceed (y/n)? " update_remote_repo
+read -rp "CONFIRMATION --> Do you wish to proceed (y/n)? " update_remote_repo
 
 # Converts whatever the user typed into lowercase characters.
-update_remote_repo=${update_remote_repo,,}
-if [[ $update_remote_repo == 'yes' || $update_remote_repo == 'y' ]]; then 
+update_remote_repo="${update_remote_repo,,}"
+if [[ "$update_remote_repo" == 'yes' || "$update_remote_repo" == 'y' ]]; then
     echo -e "\n@}-;--- Pushing the changes to the remote Git repo..."
 
     # Finally, we can push both the commit and tag to the remote repo at the same time (using the --atomic flag):
-    git push --atomic origin master $new_app_ver
+    git push --atomic origin master "$new_app_ver"
 
     echo "@}-;--- Success! The app's version details have been updated correctly."
 else
     echo -e "\n@}-;--- Git push halted as neither 'yes' or 'y' was provided as a response."
-    read -p "Would you like to UNDO the LOCAL version change Git commit and tag (y/n)? " undo_version_changes
-    undo_version_changes=${undo_version_changes,,}
-    if [[ $undo_version_changes == 'yes' || $undo_version_changes == 'y' ]]; then
-        git tag -d $new_app_ver
+    read -rp "Would you like to UNDO the LOCAL version change Git commit and tag (y/n)? " undo_version_changes
+    undo_version_changes="${undo_version_changes,,}"
+
+    if [[ "$undo_version_changes" == 'yes' || "$undo_version_changes" == 'y' ]]; then
+        git tag -d "$new_app_ver"
         git reset --soft HEAD~1
         echo -e "\n@}-;--- Undo complete! The Git tag was deleted, but the last commit has been put back to Git's --staged area to be safe."
     fi
