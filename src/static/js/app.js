4641149b87291223a9d9a215792ba3a4c94902e9
William Paul Liggett <junktext@junktext.com>
2024-03-20 19:47:55 -0400

    Version: 1.0.30

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -6,7 +6,7 @@ function App() {
                 To-Do App<br />
                 <span style={{ fontWeight: 'normal' }}>
                     {/* The version number is updated via: $ yarn version */}
-                    Modified by junktext (v1.0.29)
+                    Modified by junktext (v1.0.30)
                 </span>
             </p>
             <Container>
